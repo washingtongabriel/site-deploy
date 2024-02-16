e9d6767f6d0e0b5b89c8315550fe33c329731afc
Washington Gabriel <washingtongabriel00@gmail.com>
2024-02-16 15:56:12 -0300

    :wq

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
 })
+function exibirMensagem(event) {
+    event.preventDefault(); // Prevent page reload
+
+    document.getElementById("formulario").style.display = "none";
+    document.getElementById("mensagem-obrigado").style.display = "block";
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+
+    const dados = {
+        nome: nome,
+        email: email,
+        telefone: telefone
+    };
+
+    enviarParaWebhook(dados);
+}
